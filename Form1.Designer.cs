e33e722074ee1b9ef89d3ad0187317849c6220b1
Dima Meleshenya <dimasgithub@gmail.com>
2021-09-09 14:09:21 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace SearchAndOutputOfInformationInAFolder
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SearchDirectoryFiles = new System.Windows.Forms.FolderBrowserDialog();
+            this.DefiningADirectory = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DefiningADirectory
+            // 
+            this.DefiningADirectory.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.DefiningADirectory.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DefiningADirectory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(255)))), ((int)(((byte)(204)))));
+            this.DefiningADirectory.Location = new System.Drawing.Point(134, 87);
+            this.DefiningADirectory.Name = "DefiningADirectory";
+            this.DefiningADirectory.Size = new System.Drawing.Size(195, 38);
+            this.DefiningADirectory.TabIndex = 0;
+            this.DefiningADirectory.Text = "DefiningADirectory";
+            this.DefiningADirectory.UseVisualStyleBackColor = false;
+            this.DefiningADirectory.Click += new System.EventHandler(this.DefiningADirectory_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(465, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "A program that creates graphs based on information about your files";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(153)))));
+            this.ClientSize = new System.Drawing.Size(487, 158);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DefiningADirectory);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Вывод информации по директории";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog SearchDirectoryFiles;
+        private System.Windows.Forms.Button DefiningADirectory;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace SearchAndOutputOfInformationInAFolder
+{
+
+    public partial class Form1 : Form
+    {
+        public static string MainDirectory { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Excel.Application ExcelApplication;
+        private Excel.Workbook ExcelWorkbook;
+        private Excel.Worksheet ExcelWorksheet;
+        private void DefiningADirectory_Click(object sender, EventArgs e)
+        {
+            SearchDirectory();
+        }
+
+
+        // Метод поиска директории для получения информации о файлах; 
+        public void SearchDirectory()
+        {
+            if (SearchDirectoryFiles.ShowDialog() == DialogResult.OK) MainDirectory = SearchDirectoryFiles.SelectedPath + @"\";
+            else MainDirectory = null;
+            if (MainDirectory != null)
+            {
+                if (MessageBox.Show(text: "Вы уверены в правильности выбраной дириктории  для перемещение файлов.", caption: "Подтверждения действия", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                {
+                    MessageBox.Show(text: "Выбирите нужную директорию для записи.", caption: "Отмена пользователем", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MainDirectory = null;
+                }
+            }
+            if (MainDirectory != null) CreateExcelPage();
+        }
+        public void CreateExcelPage()
+        {
+            try
+            {
+                ExcelApplication = new Excel.Application();
+                ExcelWorkbook = ExcelApplication.Workbooks.Add(1);
+                ExcelWorksheet = (Excel.Worksheet)ExcelWorkbook.Sheets[1];
+                // Создание оглавлений;
+                ExcelWorksheet.Cells[1, "A"] = "NameFiles";
+                ExcelWorksheet.Cells[1, "B"] = "FormatFiles";
+                ExcelWorksheet.Cells[1, "C"] = "Size (MB)";
+                // Запись информации о файлах; 
+                long MediumSize = 0;
+                int Count = 2;
+                foreach (var Files in new DirectoryInfo(MainDirectory).GetFiles())
+                {
+                    ExcelWorksheet.Cells[Count, "A"] = Files.Name;
+                    ExcelWorksheet.Cells[Count, "B"] = Files.Extension;
+                    ExcelWorksheet.Cells[Count, "C"] = Files.Length / 1048576;
+                    MediumSize += Files.Length / 1048576;
+                    Count++;
+                }
+                // Средний размер файла;
+                ExcelWorksheet.Cells[Count, "D"] = "MediumSize";
+                ExcelWorksheet.Cells[Count, "E"] = MediumSize / Count - 1;
+                // Создание графиков;
+                Excel.Range chartRange;
+                // Диаграмма;
+                Excel.ChartObjects xlCharts = (Excel.ChartObjects)ExcelWorksheet.ChartObjects(Type.Missing);
+                Excel.ChartObject myChart = xlCharts.Add(10, 80, 300, 250);
+                Excel.Chart chartPage = myChart.Chart;
+                chartRange = ExcelWorksheet.get_Range("C1", $"C{Count}");
+                chartPage.SetSourceData(chartRange, Type.Missing);
+                chartPage.ChartType = Excel.XlChartType.xlColumnClustered;
+                // Вторая диаграмма 
+
+                // Сохранение Excel документа;
+                ExcelWorkbook.SaveAs(MainDirectory + "InformationAboutYouDirectory" + ".xls", Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+                ExcelApplication.Quit();
+                MessageBox.Show(text: "Процесс анализа и создания Excel документа закончено! Находится файл .xls в папке, в которой проходило иследование.", caption: "Создание выполнено успешно!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (ArgumentNullException)
+            {
+                MessageBox.Show(text: "Вы не выбрали папку для анализа", caption: "Ошибка пользователя", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(text: "Ошибка получения доступа к файлу, закройте файл из данной директивы и попробуйте снова!", caption: "Ошибка пользователя", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
